a6187342e2831621dabe3e2e40ec3570098bc8ad
capoony <capoony@gmail.com>
2012-09-08 14:05:59 +0000

    coverage_from_mpileup.py updated, cheersMartin

--- users/martin/miscelaneous_but_useful/coverage_from_mpileup.py
+++ users/martin/miscelaneous_but_useful/coverage_from_mpileup.py
@@ -19,7 +19,7 @@ This script calculates the average coverage of all populations in a pileup or mp
 #########################################################   CODE   #########################################################################
 
 parser.add_option("-m", "--mpileup", dest="m", help="mpileup file")
-parser.add_option("-n", "--names", dest="m", help="list of names of the populations in the mpileup, need to be separated by a comma; this information will be used for the header")
+parser.add_option("-n", "--names", dest="n", help="list of names of the populations in the mpileup, need to be separated by a comma; this information will be used for the header")
 parser.add_option("-s", "--samheader", dest="s", help="Header of SAM file; remember! samtools view -H")
 (options, args) = parser.parse_args()
 
