2c7a4b50922b9da6295703c5b04c6a2e951c38ea
Robert Kofler <rokofler@gmail.com>
2013-03-11 10:35:00 +0000

    updated script ro

--- users/viola/cmh-window-slider.py
+++ users/viola/cmh-window-slider.py
@@ -14,15 +14,16 @@ class CMHParser:
                 a=l.split("\t")
                 chr=a[0]
                 pos=int(a[1])
-                pval=float(a[-1])
+                pval=a[-1]
+                #2L_sim_w_su1    177693  A       234:0:0:0:0:0   174:0:0:0:0:0   236:0:0:1:0:0   176:0:0:0:0:0   133:0:0:0:0:0   194:0:0:0:0:0   284:0:0:0:0:0   166:0:0:0:0:0   108:0:4:0:0:0   40:0:2:0:0:0    NaN
                 if(pval=="NaN"):
                         return (None,None,None)
-                
+                pval=float(pval)
                 if not self.__logpvalue:
                         if(pval==0):
                                 pval=310 # max
                         else:
-                                pval= -1.0* math.log10(pval)
+                                pval= -1.0 * math.log10(pval)
                 return(chr,pos,pval)
                 
 def printwindow(chr,startpos,endpos,pvals):
@@ -34,16 +35,19 @@ def printwindow(chr,startpos,endpos,pvals):
         geomean=sum/float(count)
         print "{0}\t{1}\t{2}\t{3}".format(chr,int(midpos),count,geomean)
         
-  
+
+# /Users/robertkofler/dev/testdata/CMH_output_with_NaN.cmh.txt
+# --cmh-file /Volumes/Temp1/simcoal2/sim_haplotype_diversity/sim-res/sync/cmh-g60-hdall/N1000-hd00.cmh --window-size 100 
 
 parser = OptionParser()
 parser.add_option("--cmh-file", dest="cmh", help="A file containing the output of the cmh-test")
 parser.add_option("--window-size",dest="windowsize", help="The windowsize for the sliding window")
-parser.add_option("--logpvalue", dest="logpvalue", action="store_true", help="Is the pvalue of the cmh test in -log10?")
+parser.add_option("--logpvalue", dest="logpvalue", default=False,action="store_true", help="Is the pvalue of the cmh test in -log10?")
 (options, args) = parser.parse_args()
 
+logpvalue=options.logpvalue
 winsize=int(options.windowsize)
-parser=CMHParser(options.logpvalue)
+parser=CMHParser(logpvalue)
 
 activechr=None
 startpos=0
@@ -53,12 +57,14 @@ pvalsforwindow=[]
 for line in open(options.cmh):
         chr,pos,pval=parser.parseLine(line)
         if chr is None:
+                # skip invalid entries, eg p-value=NaN
                 continue
         
         if activechr is None:
                 activechr=chr
                 
         if activechr !=chr:
+                # a new chromosome has been encountered
                 printwindow(activechr,startpos,endpos,pvalsforwindow)
                 activechr=chr
                 startpos=0
@@ -66,16 +72,21 @@ for line in open(options.cmh):
                 pvalsforwindow=[]
         
         if pos < startpos:
+                # check if the the file is sorted; This pos< startpos should not happen if it is sorted
                 raise ValueError("cmh file needs to be sorted by start position for" +chr + " " +activechr+" "+str(pos)+" " +str(startpos))
                 
         if(pos>endpos):
+                # the position of the SNP exceeds the window position -> print and reshift window
                 printwindow(activechr,startpos,endpos,pvalsforwindow)
                 while(pos>endpos):
                         startpos+=winsize
                         endpos+=winsize
                 pvalsforwindow=[]
+                
         # in any case the new pvalue needs to be appended somewhere        
         pvalsforwindow.append(pval)
+        
+# Also do not forget to print the last window
 printwindow(activechr,startpos,endpos,pvalsforwindow)
         
                 
