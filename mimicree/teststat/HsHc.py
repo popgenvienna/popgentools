92cc81cbbb185ec255c0a86fb06c88ce2769ef02
Robert Kofler <rokofler@gmail.com>
2014-03-24 12:02:44 +0000

    novel updated teststatistics ro

---
+++ mimicree/teststat/HsHc.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+import sys
+import random
+from optparse import OptionParser, OptionGroup
+import collections
+from syncIO import SyncReaderMajMin
+
+
+
+        
+
+def parse_line(entries,baselist,derivedlist):
+	# (C,A),(108,45),(90,47))
+	parsed=entries
+	
+	derc=[0,0]
+	basec=[0,0]
+	for i in derivedlist:
+		key=i-1
+		derc[0]+=parsed[key][0]
+		derc[1]+=parsed[key][1]
+	for i in baselist:
+		key=i-1
+		basec[0]+=parsed[key][0]
+		basec[1]+=parsed[key][1]
+	return (basec,derc)
+
+"""
+def nover2(n):
+	nf=float(n)
+	ret=(nf*(nf-1.0))/2.0
+	return ret
+
+def computeHeterozygosit(maj,min):
+	cov=maj+min
+	h=(nover2(cov)-nover2(maj)-nover2(min))/nover2(cov)
+	return h
+"""
+
+def computeHeterozygosity(c):
+	cov=c[0]+c[1]
+	if cov==0:
+		return 0.0
+	fcov=float(cov)
+	fmaj=float(c[0])/fcov
+	fmin=float(c[1])/fcov
+	h=1-fmaj**2.0-fmin**2.0
+	return h
+
+def computeDivergence(c1,c2):
+	cov1=c1[0]+c1[1]
+	cov2=c2[0]+c2[1]
+	if cov1==0 or cov2==0:
+		return 0.0
+	sumcomp=float(cov1*cov2)
+	summaj=float(c1[0]*c2[0])
+	summin=float(c1[1]*c2[1])
+	div=(sumcomp-summaj-summin)/sumcomp
+	return div
+
+
+def get_hshc(basec,derc):
+	hs=computeHeterozygosity(derc)
+	hc=computeHeterozygosity(basec)
+	if hc<0.00000000000001:
+		return 0.0
+	return hs/hc
+
+def parse_comparestring(comp):
+	a=comp.split(",")
+	
+	baselist=[]
+	derivedlist=[]
+	for e in a:
+		b,d=map(int,e.split("-"))
+		baselist.append(b)
+		derivedlist.append(d)
+	return (baselist,derivedlist)
+		
+
+parser = OptionParser()
+parser.add_option("--sync",dest="sync",help="A file containing the cmh results")
+parser.add_option("--compare",dest="comp",help="A comparision string as with the cmh-test")
+(options, args) = parser.parse_args()
+
+baselist,derivedlist=parse_comparestring(options.comp)
+
+for chr,pos,mami,s in SyncReaderMajMin(options.sync):
+	# [2L,15,(C,A),(108,45),(90,47))
+	basec,derc=parse_line(s,baselist,derivedlist)
+	hshc=get_hshc(basec,derc)
+	
+	topr=[chr,str(pos),str(hshc)]
+	print "\t".join(topr)
+        
--- mimicree/teststat/diffstat.py
+++ mimicree/teststat/diffstat.py
@@ -3,70 +3,7 @@ import sys
 import random
 from optparse import OptionParser, OptionGroup
 import collections
-
-class SyncReader:
-	"""
-	Light weight sync reader
-	2L	15	A	45:0:108:0:0:0	47:0:90:0:0:0	52:0:103:0:0:0	58:0:91:0:0:0	57:0:107:0:0:0	49:0:84:0:0:0
-	2L	47	A	155:0:0:0:0:0	141:0:0:0:0:0	152:0:0:0:0:0	146:0:0:0:0:0	161:0:1:0:0:0	172:0:0:0:0:0
-	2L	57	A	148:0:4:0:0:0	128:0:4:0:0:0	172:0:4:0:0:0	164:0:1:0:0:0	148:0:4:0:0:0	141:0:1:0:0:0
-	2L	89	A	156:0:3:0:0:0	161:0:6:0:0:0	146:0:5:0:0:0	158:0:4:0:0:0	163:0:6:0:0:0	151:0:0:0:0:0
-	2L	224	A	154:0:0:0:0:0	149:0:0:0:0:0	135:0:0:0:0:0	167:0:0:0:0:0	164:0:0:0:0:0	162:0:0:0:0:0
-	"""
-	def __init__(self,file):
-		self.__filename=file
-		self.__filehandle=open(file,"r")
-	
-
-	def __iter__(self):
-		return self
-	
-	def next(self):
-		line=""
-		while(1):
-			line=self.__filehandle.readline()
-			if line=="":
-				raise StopIteration
-			line=line.rstrip('\n')
-			if line != "":
-				break
-		
-		a=line.split()
-		chr=a.pop(0)
-		pos=a.pop(0)
-		refc=a.pop(0)
-		majmin=SyncReader.parse_sync(a)
-		return (chr,pos,majmin)
-
-		
-	@classmethod
-	def parse_sync(cls,entries):
-		parsed=[]
-		for e in entries:
-			a=map(float,e.split(":"))
-			np={'A':a[0],'T':a[1],'C':a[2],'G':a[3]}
-			parsed.append(np)
-		ac,tc,cc,gc = (0,0,0,0)
-		
-		for p in parsed:
-			ac += p['A']
-			tc += p['T']
-			cc += p['C']
-			gc += p['G']
-			
-		tmpar=[ (ac,'A'),
-			(tc,'T'),
-			(cc,'C'),
-			(gc,'G') ]
-		
-		tmpar=sorted(tmpar, key=lambda cs: -cs[0])
-		major=tmpar[0][1]
-		minor=tmpar[1][1]
-		toret=[]
-		for p in parsed:
-			novel=(p[major],p[minor])
-			toret.append(novel)
-		return toret
+from syncIO import SyncReaderMajMin
 
         
 
@@ -128,9 +65,9 @@ parser.add_option("--compare",dest="comp",help="A comparision string as with the
 
 baselist,derivedlist=parse_comparestring(options.comp)
 
-for chr,pos,s in SyncReader(options.sync):
+for chr,pos,mami,s in SyncReaderMajMin(options.sync):
         baseentries,derivedentries=parse_line(s,baselist,derivedlist)
         assstat=get_diffstat(baseentries,derivedentries)
         
-        topr=[chr,pos,str(assstat)]
+        topr=[chr,str(pos),str(assstat)]
         print "\t".join(topr)
