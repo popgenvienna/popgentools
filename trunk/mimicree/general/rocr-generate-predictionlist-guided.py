ba5efa6e00d26033097c102f5d804fe78ad8db79
RoKofler <RoKofler@4a3a2c13-8525-4b7f-8644-744f5788dcc6>
2014-09-01 12:54:23 +0000

    added guided prediction script ro

---
+++ trunk/mimicree/general/rocr-generate-predictionlist-guided.py
@@ -0,0 +1,69 @@
+import sys
+import random
+from optparse import OptionParser, OptionGroup
+import collections
+import gzip
+
+
+class ResultReader:
+	def __init__(self,file):
+		self.__file=file
+		fh=None
+		if(chrguidefile.endswith(".gz")):
+			fh=gzip.open(file,mode='rb')
+		else:
+			fh=open(file)
+		self.__fh=fh
+		self.__buffer=""
+		self.__lastline=""
+		
+	def __iter__(self):
+		return self
+	
+	def __getnext(self):
+		toret=""
+		if(self.__buffer!=""):
+			toret=self.__buffer
+			self.__buffer=""
+		else:
+			return self.__fh.readline()
+			
+			
+	def buffer(self):
+		self.__buffer=self.__lastline
+	
+	def next(self):
+		line=self.__getnext();
+		if line=="":
+			raise StopIteration
+		self.__lastline=line
+		line=line.rstrip('\n');
+		a=line.split()
+		chr=a[0]
+		pos=a[1]
+		sig=a[-1]
+		return (chr,pos,sig)
+
+guidereader=ResultReader(sys.argv[1])
+readar=[]
+for i in range(2,len(sys.argv)):
+	filename=sys.argv[i]
+	readar.append(ResultReader(filename))
+
+
+for activechr,activepos,sig in guidereader:
+	topr=[]
+	for i in range(0,len(readar)):
+		chr,pos,sig=readar[i].next()
+		if(chr==activechr and pos==activepos):
+			topr.append(sig)
+		else:
+			topr.append("0.0")
+			reader[i].buffer()
+	toprstr="\t".join(topr)
+	print(toprstr)
+	
+	
+	
+	
+	
