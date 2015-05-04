a3f205446378e1a38a611af9709ba07f28e44e70
Robert Kofler <rokofler@gmail.com>
2015-05-04 16:15:00 +0000

    changed driver ro

--- TE/driver/map-folder.sh
+++ TE/driver/map-folder.sh
@@ -28,8 +28,8 @@ do
     	n=${n%_1.fq.gz}
 	m1="bwa bwasw -t 12 $reference  $infolder/${n}_1.fq.gz > $outfolder/raw/${n}_1.sam  2> /dev/null"
 	m2="bwa bwasw -t 12 $reference  $infolder/${n}_2.fq.gz > $outfolder/raw/${n}_2.sam  2> /dev/null"
-	m3="perl /Volumes/Temp2/Robert/popoolationte/samro.pl --fq1 $infolder/${n}_1.fq.gz --fq2 $infolder/${n}_2.fq.gz --sam1 $outfolder/raw/${n}_1.sam --sam2 --output /dev/stdout | samtools view -Sb - | samtools sort -m 4G - $outfolder/${n}.sort"
-	echo $m1
-	echo $m2
-	echo $m3 
+	m3="perl /Volumes/Temp2/Robert/popoolationte/samro.pl --fq1 $infolder/${n}_1.fq.gz --fq2 $infolder/${n}_2.fq.gz --sam1 $outfolder/raw/${n}_1.sam --sam2 $outfolder/raw/${n}_2.sam --output /dev/stdout | samtools view -Sb - | samtools sort -m 4G - $outfolder/${n}.sort"
+	eval $m1
+	eval $m2
+	eval $m3 
 done
