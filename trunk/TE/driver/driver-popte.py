837f40a95abb12ca4c2caed881220bd151207d5a
rokofler <rokofler@4a3a2c13-8525-4b7f-8644-744f5788dcc6>
2015-05-10 12:39:45 +0000

    some changes made ro

--- trunk/TE/driver/driver-popte.py
+++ trunk/TE/driver/driver-popte.py
@@ -40,40 +40,50 @@ mkdir -p $outfolder/stat
 
 
 # sam/bam
-if [ $sam == "*.bam" ] 
+if [[ $sam =~ \.bam$ ]] 
 then
 echo "converting ${sam} to ${outfolder}/tmp/input.sam"
 samtools view $sam > $outfolder/tmp/input.sam
-sam=$outfolder/tmp/input.sam
+else
+ln $sam $outfolder/tmp/input.sam
 fi
+sam=$outfolder/tmp/input.sam
+
 
 # let's do it
-perl $popte/identify-te-insertsites.pl --input $sam  --te-hierarchy-file $hier --te-hierarchy-level family --min-count 1 --min-map-qual 15 --narrow-range 100 --output $outfolder/tmp/tmp.tedirectional
+perl $popte/identify-te-insertsites.pl --input $sam  --te-hierarchy-file $hier --te-hierarchy-level family --min-count 1 --min-map-qual 15 --narrow-range 100 --output $outfolder/tmp/tmp.tedirectional > /dev/null
 # N-s
-perl $popte/genomic-N-2gtf.pl --input $ref > $outfolder/tmp/polyn.gtf
+perl $popte/genomic-N-2gtf.pl --input $genome > $outfolder/tmp/polyn.gtf
 # crosslink
-perl $popte/crosslink-te-sites.pl --directional-insertions $outfolder/tmp/tmp.tedirectional --min-dist 85 --max-dist 400 --output $outfolder/tmp/tmp.teinsertions --poly-n $outfolder/tmp/polyn.gtf --te-hierarchy $hier
+perl $popte/crosslink-te-sites.pl --directional-insertions $outfolder/tmp/tmp.tedirectional --min-dist 85 --max-dist 400 --output $outfolder/tmp/tmp.teinsertions --poly-n $outfolder/tmp/polyn.gtf --te-hierarchy $hier > /dev/null
 # estimate poly
-perl ~r/popoolationte/estimate-polymorphism.pl --sam-file mapte/Dsim/Dsim.sort.sam --te-insert-file te/Dsim/raw/Dsim.teinsertions --te-hierarchy-file ~dmau/refg/Dsim/Dsim-combinedte-incM.hier --te-hierarchy-level family --min-map-qual 15 --output $outfolder/tmp/tmp.polytes
+
+perl $popte/estimate-polymorphism.pl --sam-file mapte/Dsim/Dsim.sort.sam --te-insert-file te/Dsim/raw/Dsim.teinsertions --te-hierarchy-file ~dmau/refg/Dsim/Dsim-combinedte-incM.hier --te-hierarchy-level family --min-map-qual 15 --output $outfolder/tmp/tmp.polytes > /dev/null
 
 # filter major chromosome 
 cat $outfolder/tmp/tmp.polytes |awk '$1=="X" || $1=="2L" || $1=="2R" || $1=="3L" || $1=="3R" || $1=="4"' > $outfolder/tmp/tmp.euchr.polytes
 perl $pgt/TE/filter/filter-teinserts.pl --te-insertions $outfolder/tmp/tmp.euchr.polytes --min-count 10 --output $outfolder/results/${prefix}.mc10.euchr.polytes
 perl $pgt/TE/filter/filter-teinserts.pl --discard-overlapping --te-insertions $outfolder/results/${prefix}.mc10.euchr.polytes --output  $outfolder/results/${prefix}.mc10.nooverlap.euchr.polytes 
 
-for $b in $bootstrap
+for b in $bootstrap
 do
- python $pgt/TE/TE/melsim/targetcoverage.py --tes $outfolder/results/${prefix}.mc10.euchr.polytes --target-cov $b --min-count > $outfolder/bootstrap/${prefix}.boot${b}.polytes
+ python $pgt/TE/melsim/targetcoverage.py --tes $outfolder/results/${prefix}.mc10.euchr.polytes --target-cov $b --min-count 1 > $outfolder/bootstrap/${prefix}.boot${b}.polytes
  perl $pgt/TE/filter/filter-teinserts.pl --discard-overlapping --te-insertions $outfolder/bootstrap/${prefix}.boot${b}.polytes --output $outfolder/bootstrap/${prefix}.boot${b}.nooverlap.polytes
  python $pgt/TE/mapstatdebug/insertcount-famwise.py --tes $outfolder/bootstrap/${prefix}.boot${b}.polytes > $outfolder/stat/${prefix}.boot${b}.inscount.txt
 python $pgt/TE/mapstatdebug/insertcount-famwise.py --tes $outfolder/bootstrap/${prefix}.boot${b}.nooverlap.polytes > $outfolder/stat/${prefix}.boot${b}.nooverlap.inscount.txt
 done
 
 # statistics
-perl $pgt/TE/mapstatdebug/debugstat-mapedreads-familywise.pl --input $sam --te-annotation $hier > $outfolder/stat/${prefix}.mapedreads.txt
-perl $pgt/TE/mapstatdebug/debugstat-presFrag-familywise.pl --input $sam --te-annotation $hier > $outfolder/stat/${prefix}.presfrag.txt
+stat1="perl ${pgt}/TE/mapstatdebug/debugstat-mapedreads-familywise.pl --input ${sam} --te-annotation ${hier} > ${outfolder}/stat/${prefix}.mapedreads.txt"
+#echo $stat1
+eval $stat1
+stat2="perl ${pgt}/TE/mapstatdebug/debugstat-presFrag-familywise.pl --input ${sam} --te-annotation ${hier} > ${outfolder}/stat/${prefix}.presfrag.txt"
+#echo $stat2
+eval $stat2
 python $pgt/TE/mapstatdebug/insertcount-famwise.py --tes $outfolder/results/${prefix}.mc10.euchr.polytes > $outfolder/stat/${prefix}.mc10.euchr.inscount.txt
 python $pgt/TE/mapstatdebug/insertcount-famwise.py --tes $outfolder/results/${prefix}.mc10.nooverlap.euchr.polytes > $outfolder/stat/${prefix}.mc10.nooverlap.euchr.inscount.txt
 
+#clean 
+rm $outfolder/tmp/input.sam
 
 
