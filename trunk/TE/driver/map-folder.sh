b59d985f400cd0be1093a2d7bea8fb81862f6c59
rokofler <rokofler@4a3a2c13-8525-4b7f-8644-744f5788dcc6>
2015-05-10 11:28:35 +0000

    added popdriver ro

--- trunk/TE/driver/map-folder.sh
+++ trunk/TE/driver/map-folder.sh
@@ -17,8 +17,7 @@ samro=""
 
 
 # folders
-mkdir $outfolder
-mkdir $outfolder/raw
+mkdir -p $outfolder/raw
 
 # paths
 for f in $infolder/*_1.fq.gz
@@ -29,7 +28,10 @@ do
 	m1="bwa bwasw -t 12 $reference  $infolder/${n}_1.fq.gz > $outfolder/raw/${n}_1.sam  2> /dev/null"
 	m2="bwa bwasw -t 12 $reference  $infolder/${n}_2.fq.gz > $outfolder/raw/${n}_2.sam  2> /dev/null"
 	m3="perl /Volumes/Temp2/Robert/popoolationte/samro.pl --fq1 $infolder/${n}_1.fq.gz --fq2 $infolder/${n}_2.fq.gz --sam1 $outfolder/raw/${n}_1.sam --sam2 $outfolder/raw/${n}_2.sam --output /dev/stdout | samtools view -Sb - | samtools sort -m 4G - $outfolder/${n}.sort"
+	echo $m1	
 	eval $m1
+	echo $m2	
 	eval $m2
+	echo $m3
 	eval $m3 
 done
