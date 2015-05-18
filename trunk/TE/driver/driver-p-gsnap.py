97114968b5cf3e8fd2c5bd026a4f8d1c56565ea0
rokofler <rokofler@4a3a2c13-8525-4b7f-8644-744f5788dcc6>
2015-05-18 13:32:56 +0000

    added script ro

---
+++ trunk/TE/driver/driver-p-gsnap.py
@@ -0,0 +1,45 @@
+#!/bin/zsh
+# read parameters
+if [ $# -ne 3 ]
+  # "$#" is number of parameters- here we test
+  # whether it is not equal to two
+   then
+   echo "Usage $0 input.bam outputdirname prefix"
+exit 2
+fi
+
+
+source ~/.zshrc
+set -o shwordsplit
+
+sam=$1
+genomedir="/Volumes/Temp/Robert/ee-pele/refg/pelement/gsnap"
+outfolder=$2
+prefix=$3
+
+echo "using bam ${sam}"
+echo "using outfolder ${outfolder}"
+echo "using prefix ${prefix}"
+
+# set paths
+pgt="/Volumes/Temp2/Robert/popgentools"
+popte="/Volumes/Temp2/Robert/popoolationte"
+
+s2f="${pgt}/TE/melsim/sam2fastq.py"
+
+# make folders
+mkdir -p $outfolder
+mkdir -p $outfolder/raw
+
+# let's do it
+
+samtools index $sam
+samtools view $sam PPI251 > $outfolder/raw/tmp.sam
+python $s2f $outfolder/raw/tmp.sam > $outfolder/raw/tmp.fastq
+co1="gsnap -d pele -D ${genomedir} -A sam --novelsplicing=1 -t 4 --quality-protocol illumina ${outfolder}/raw/tmp.fastq | samtools view -Sb - |samtools sort - ${outfolder}/${prefix}.sort"
+eval $co1
+
+samtools index $outfolder/${prefix}.sort.bam
+rm -rf $outfolder/raw
+
+
